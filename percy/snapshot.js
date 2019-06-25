f9719a3eb93a52f4568e9a900f0812188056cc6e
Pointereau, Pierre Yves <PierreYves.Pointereau@news.com.au>
2019-06-25 12:20:59 +1000

    Percy test

--- percy/snapshot.js
+++ percy/snapshot.js
@@ -3,7 +3,7 @@ const PercyScript = require('@percy/script');
 // const knobsTabSelector = '#storybook-panel-root > div:nth-child(1) button:nth-child(2)';
 
 PercyScript.run(async (page, percySnapshot) => {
-    // await page.goto('http://localhost:63874/?path=/story/navigation--simple');
+    // await page.goto('http://localhost:61026/?path=/story/navigation--simple');
     // await page.waitFor('a[title="Navigation"]');
     // await page.click('a[title="Navigation - Simple"]');
     // await percySnapshot('Navigation - Simple - nca');
@@ -17,18 +17,18 @@ PercyScript.run(async (page, percySnapshot) => {
     // await page.select('select[name="Brand style"]', 'kidspot');
     // await percySnapshot('Navigation - Simple - kidspot');
 
-    await page.goto('http://localhost:63874/iframe.html?id=navigation--navigation-simple&knob-Brand%20style=nca');
+    await page.goto('http://localhost:61026/iframe.html?id=navigation--navigation-simple&knob-Brand%20style=nca');
     await percySnapshot('Navigation - Simple - nca');
 
-    await page.goto('http://localhost:63874/iframe.html?id=navigation--navigation-simple&knob-Brand%20style=mhr');
+    await page.goto('http://localhost:61026/iframe.html?id=navigation--navigation-simple&knob-Brand%20style=mhr');
     await percySnapshot('Navigation - Simple - mhr');
 
-    await page.goto('http://localhost:63874/iframe.html?id=navigation--navigation-simple&knob-Brand%20style=bodyandsoul');
+    await page.goto('http://localhost:61026/iframe.html?id=navigation--navigation-simple&knob-Brand%20style=bodyandsoul');
     await percySnapshot('Navigation - Simple - bodyandsoul');
 
-    await page.goto('http://localhost:63874/iframe.html?id=navigation--navigation-simple&knob-Brand%20style=whimn');
+    await page.goto('http://localhost:61026/iframe.html?id=navigation--navigation-simple&knob-Brand%20style=whimn');
     await percySnapshot('Navigation - Simple - whimn');
 
-    await page.goto('http://localhost:63874/iframe.html?id=navigation--navigation-simple&knob-Brand%20style=kidspot');
+    await page.goto('http://localhost:61026/iframe.html?id=navigation--navigation-simple&knob-Brand%20style=kidspot');
     await percySnapshot('Navigation - Simple - kidspot');
 });
